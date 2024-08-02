1dabba48d3a88e85e450bd9a984c3011803a7c46
yhsben88 <hiusum.yuen@ucalgary.ca>
2024-08-01 19:38:08 -0600

    init: draft for home page

--- tinder-clone/src/pages/Home.js
+++ tinder-clone/src/pages/Home.js
@@ -1,7 +1,18 @@
+
+
 const Home=() => {
+
+    const authToken = false
+
+    const handleClick = () =>{
+        console.log("clicked")
+    }
     return (
-        <div>
-            home
+        <div className="home">
+            <h1>Swipe Right@</h1>
+            <button className="primary-button" onClick={handleClick}>
+                {authToken ? "Signout" : "Create Account"}
+            </button>
         </div>
     );
 };
