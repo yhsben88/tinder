f7c263fe23e34d88d183fcb61f164e3a1ac9c639
yhsben88 <hiusum.yuen@ucalgary.ca>
2024-08-06 15:44:04 -0600

    feat: include navigation bar

--- tinder-clone/src/components/Nav.js
+++ tinder-clone/src/components/Nav.js
@@ -1,6 +1,15 @@
-const Nav=()=>{
+import colorlogo from '../images/color-tinder-logo.png'
+import whitelogo from '../images/white-tinder-logo.png'
+
+const Nav=({minimal, authToken})=>{
     return(
-        <div>Nav</div>
+        <nav>
+            <div className="logo-container">
+                <img className="logo" src={ minimal ? colorlogo : whitelogo } alt="tinder logo"/>
+            </div>
+
+            {!authToken && <button className="nav-button"> Log in</button>}
+        </nav>
     )
 }
 export default Nav;
--- tinder-clone/src/pages/Home.js
+++ tinder-clone/src/pages/Home.js
@@ -9,15 +9,15 @@ const Home=() => {
         console.log("clicked")
     }
     return (
-        <>
-        <Nav/>
+        <div className="overlay">
+        <Nav minimal={false} authToken={authToken}/>
         <div className="home">
             <h1>Swipe Right@</h1>
             <button className="primary-button" onClick={handleClick}>
                 {authToken ? "Signout" : "Create Account"}
             </button>
         </div>
-        </>
+        </div>
     );
 };
 
